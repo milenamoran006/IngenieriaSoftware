e20544fffcf47babb7bd0a2533e4dddcdba20fe5
milenamoran006 <milenajamnia@gmail.com>
2024-11-13 20:15:32 +0000

    clase Arco

---
+++ JuegoLucha/Arco.java
@@ -0,0 +1,9 @@
+class Arco implements Arma {
+    public int generarDano() {
+        return new Random().nextInt(30) + 15; // Daño entre 15 y 30
+    }
+    
+    public String getNombre() {
+        return "Arco";
+    }
+}
